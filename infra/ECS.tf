b5d5e64156de9f843640c8a7eef770217a01a4dc
Julio Dutra <jjcutra@gmail.com>
2022-06-21 14:40:05 -0300

    Configurar task definition

--- infra/ECS.tf
+++ infra/ECS.tf
@@ -11,3 +11,29 @@ module "ecs" {
     }
   }
 }
+
+resource "aws_ecs_task_definition" "Django-API" {
+  family                   = "Django-API"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.cargo.arn
+  container_definitions = jsoncode(
+    [
+      {
+        "name"      = "producao",
+        "image"     = "095878108579.dkr.ecr.us-west-2.amazonaws.com/producao:v1",
+        "cpu"       = 256,
+        "memory"    = 512,
+        "essential" = true
+        "portMappings" = [
+          {
+            "containerPort" = 8000,
+            "hostPort"      = 8000
+          }
+        ]
+      }
+    ]
+  )
+}
