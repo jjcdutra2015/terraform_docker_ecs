45c9142581e7a6377d4ec93cd0ebb080ef3d44e0
Julio Dutra <jjcutra@gmail.com>
2022-06-21 10:30:40 -0300

    Criar ALB listener

--- infra/ALB.tf
+++ infra/ALB.tf
@@ -3,3 +3,14 @@ resource "aws_lb" "alb" {
   security_groups = [aws_security_group.alb.id]
   subnets         = [module.vpc.public_subnets]
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "8000"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
